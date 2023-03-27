f81e29a07783741ed109cf9e7f298e0143b99014
sergiomc97 <carrascosa_96@hotmail.es>
2023-03-27 18:44:48 +0200

    init commit

---
+++ src/main/java/com/NextTask/CrudNextTask/CrudNextTaskApplication.java
@@ -0,0 +1,13 @@
+package com.NextTask.CrudNextTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudNextTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudNextTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NextTask/CrudNextTask/CrudNextTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NextTask.CrudNextTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudNextTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
